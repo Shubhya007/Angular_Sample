78c9d8b472b812645822765331828d66c3a47af9
Shubham Shinde <39937367+Shubhya007@users.noreply.github.com>
2020-07-25 17:01:12 +0530

    video 3 is complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Sample';
+  title:string;
+  city:string;
+  time:string;
+  name:string;
+
+  constructor(){
+      this.title = 'Shubham Shinde'
+      this.city = 'Mumbai'
+      this.time = new Date().toLocaleTimeString();
+      this.name = '';
+  }
+
+  updateTime():void{
+      this.time= new Date().toLocaleTimeString();
+  }
 }
