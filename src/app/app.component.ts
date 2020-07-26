71b80de7ddfb73db412c1dfdbdbec092d2c23112
Shubham Shinde <39937367+Shubhya007@users.noreply.github.com>
2020-07-26 21:23:45 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Employee } from '../models/employee';
+import { CalculatorService } from "../services/calculator.service"
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[CalculatorService]
 })
+
 export class AppComponent {
   title: string;
   city: string;
@@ -12,18 +17,28 @@ export class AppComponent {
   fontWeight: string;
   fontStyle: string;
   fontSize: string;
+  employee: Array<Employee>
 
-  constructor() {
+  constructor(private calcObj:CalculatorService) {
     this.title = 'Shubham Shinde'
     this.city = 'Mumbai'
     this.name = '';
     this.fontWeight = "normal";
     this.fontStyle = "normal";
     this.fontSize = "20px";
+    this.employee = [
+      { id: 1, name: 'Shubham Shinde' },
+      { id: 2, name: 'Ragnar Loathbroke' },
+      { id: 3, name: 'Thor' },
+      { id: 1, name: 'Shubham Shinde' },
+      { id: 2, name: 'Ragnar Loathbroke' }
+      
+    ];
+    console.log(this.calcObj.add(5,5));
   }
-  updateSize(e):void{
+  updateSize(e): void {
     var element = e.target;
-    var size = element.value+'px';
+    var size = element.value + 'px';
     this.fontSize = size;
   }
 }
